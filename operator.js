ab2ac4edae91762961e601a170e64b886da0ceb8
Manish Ghatori <manishgahtori57@gmail.com>
2023-06-04 03:17:08 +0530

    chapter 5 lesson 4

---
+++ operator.js
@@ -0,0 +1,12 @@
+function operator(){
+    var age = 25;
+    var gender = "male";
+    
+    if (age > 18 && gender === "male") {
+      console.log("and operator");
+    } else {
+      console.log("none");
+    }
+}    
+operator();
+
